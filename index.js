759d1467637709bbcb7ebeab7086c29afee9b65e
Hicham NAIMI <hicham.naimi@jdc.fr>
2018-04-13 10:04:41 +0200

    minor bug fix

--- index.js
+++ index.js
@@ -1,38 +1,40 @@
-export default memee = (callback, {maxAge} = 0) => {
-  let cache = {};
-  const queueOfCleaners = [];
-  let timeout = null;
-  const name = callback.name;
-  let args = null;
-  const validMaxAge = isNaN(parseInt(maxAge, 10)) ? 0 : maxAge;
+(function() {
+  var memee = (callback, {maxAge} = 0) => {
+    let cache = {};
+    const queueOfCleaners = [];
+    let timeout = null;
+    const name = callback.name;
+    let args = null;
+    const validMaxAge = isNaN(parseInt(maxAge, 10)) ? 0 : maxAge;
 
-  _queueCleaners = () => {
-    queueOfCleaners.push({ time: validMaxAge, value: `${name+args}` });
-    queueOfCleaners.sort((a, b) => a.time > b.time ? 1 : -1);
-  }
-
-  const _memorize = () => {
-    const callbackResult = callback.apply(this, args);
-    cache[`${name+args}`] = callbackResult ? callbackResult : true;
-    _queueCleaners();
-    setTimeout(_clearCache, validMaxAge);
-  }
-
-  const _clearCache = () => {
-    const concernedTimeout = queueOfCleaners.splice(0, 1)[0].value;
-    delete cache[concernedTimeout];
-  }
-
-  return (...funcArgs) => {
-    args = funcArgs.length ? [...funcArgs] : 'none';
-    if (!cache[`${name+args}`]) {
-      _memorize();
+    _queueCleaners = () => {
+      queueOfCleaners.push({ time: validMaxAge, value: `${name+args}` });
+      queueOfCleaners.sort((a, b) => a.time > b.time ? 1 : -1);
     }
-    return cache[`${name+args}`];
-  };
-}
 
-if (typeof exports === 'object' && typeof exports.nodeName !== 'string') {
-  module.exports = memee;
-  return '';
-}
+    const _memorize = () => {
+      const callbackResult = callback.apply(this, args);
+      cache[`${name+args}`] = callbackResult ? callbackResult : true;
+      _queueCleaners();
+      setTimeout(_clearCache, validMaxAge);
+    }
+
+    const _clearCache = () => {
+      const concernedTimeout = queueOfCleaners.splice(0, 1)[0].value;
+      delete cache[concernedTimeout];
+    }
+
+    return (...funcArgs) => {
+      args = funcArgs.length ? [...funcArgs] : 'none';
+      if (!cache[`${name+args}`]) {
+        _memorize();
+      }
+      return cache[`${name+args}`];
+    };
+  }
+
+  if (typeof module !== 'undefined' && typeof module.exports !== 'undefined')
+    module.exports = Validator;
+  else
+    window.memee = memee;
+})(this);
