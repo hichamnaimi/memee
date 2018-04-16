ef87d587b9397bb3e445712aff7c8db13d9722e3
Hicham NAIMI <hicham.naimi@jdc.fr>
2018-04-16 12:08:56 +0200

    modularized project for supporting multiple options behavior

--- index.js
+++ index.js
@@ -17,9 +17,7 @@ const initGlobals = (callback) => {
 }
 
 const initMemoize = (maxAge) => {
-  const { callback } = globals;
-  memoize.cache = {};
-  memoize.queueOfCleaners = [];
+  const { callback, memoize } = globals;
   memoize.callbackName = callback.name;
   memoize.validMaxAge = isNaN(parseInt(maxAge, 10)) ? 0 : maxAge;
 }
@@ -29,28 +27,30 @@ const initDebounce = () => {
 }
 
 // (IEF) memoization functionalities
-const memoize = ((args) => {
+const memoize = (() => {
+
+  const _clearCache = () => {
+    let { cache, queueOfCleaners } = globals.memoize;
+    const concernedTimeout = queueOfCleaners.splice(0, 1)[0].value;
+    console.log('deleting cache');
+    delete cache[concernedTimeout];
+    console.log('cache content: ', cache);
+  }
 
   const _queueCleaners = () => {
     const { args } = globals;
-    const { validMaxAge, callbackName } = globals.memoize;
+    const { validMaxAge, callbackName, queueOfCleaners } = globals.memoize;
     queueOfCleaners.push({ time: validMaxAge, value: `${callbackName+args}` });
     queueOfCleaners.sort((a, b) => a.time > b.time ? 1 : -1);
   }
 
   const _memorize = () => {
     const { callback, args } = globals;
-    const { callbackName, validMaxAge } = globals.memoize;
+    const { callbackName, validMaxAge, cache } = globals.memoize;
     const callbackResult = callback.apply(this, [...args]);
     cache[`${callbackName+args}`] = callbackResult ? callbackResult : true;
     _queueCleaners();
-    setTimeout(_clearCache, validMaxAge);
-  }
-
-  const _clearCache = () => {
-    const { cache } = globals.memoize;
-    const concernedTimeout = queueOfCleaners.splice(0, 1)[0].value;
-    delete cache[concernedTimeout];
+    if (validMaxAge) setTimeout(_clearCache, validMaxAge);
   }
 
   // entry point
@@ -58,6 +58,7 @@ const memoize = ((args) => {
     let { args } = globals;
     let { callbackName, cache } = globals.memoize;
     if (!cache[`${callbackName+args}`]) {
+    	console.log('not cached');
       _memorize();
     }
     return cache[`${callbackName+args}`];
@@ -71,13 +72,14 @@ const memoize = ((args) => {
 
 
 
-module.exports = (callback, { maxAge = 0, debounced: false } = 0) => {
+module.exports = (callback, { maxAge = 0, debounced= false } = 0) => {
   // init globals
   initGlobals(callback);
   initMemoize(maxAge);
 
   return (...args) => {
     globals.args = args.length ? args : 'none';
-    memorize.applyMemoization();
+    // applying memoization
+    memoize.applyMemoization();
   };
 }
